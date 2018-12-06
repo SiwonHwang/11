65153304486fbbd89d77cf3a6b8b5f27eb8a4bcb
SiwonHwang <kimshluv@naver.com>
2018-12-06 15:41:20 +0900

    04

--- linkedList.c
+++ linkedList.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct linknd {
 	int data;
@@ -7,3 +9,22 @@ typedef struct linknd {
 } linknd_t; 
 
 static linknd_t *list; //linked List 실체 
+
+linknd_t* create_node(int value) {
+	
+	linknd_t* ndPtr;
+	
+	//동적 메모리 할당
+	ndPtr = (linknd_t*)malloc( sizeof(linknd_t) );
+	if (ndPtr == NULL)
+	{
+		printf("ERROR\n");
+		return NULL;
+	}
+	
+	//정수값 저장 
+	ndPtr->data = value;
+	ndPtr->next = NULL;
+	
+	return ndPtr;
+}
